7a5364e9f65f2674f6a197d1fda7e46772fd1b2e
duanitin <36550206+duanitin@users.noreply.github.com>
2018-02-18 14:14:58 -0800

    added new information page

--- resources/views/information.blade.php
+++ resources/views/information.blade.php
@@ -2,7 +2,13 @@
 
 
 @section('page')
-    <div id="thumbwrapper">
-    Test
+    <div>
+    <form id="form1" method="post">
+    First Name: <input type="text" name="fname"><br>
+    Last Name: <input type="text" name="lname"><br>
+    Email:<input type="text" name="email"><br>
+    Phone:<input type="text" name="phone"><br>
+    <input type="submit" value="Submit">
+    </form>
     </div>
 @endsection
