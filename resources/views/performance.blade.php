56317aa990de68fc431ce0ececda2afe90c42a6d
David Craig <contact@davidcraig.us>
2018-02-18 13:54:58 -0800

    Added phoney baloney specs

--- resources/views/performance.blade.php
+++ resources/views/performance.blade.php
@@ -2,7 +2,18 @@
 
 
 @section('page')
-    <div id="thumbwrapper">
-    Test
-    </div>
+<h4>SG4 "Wimpy"</h4>
+<table>
+	<tr><td>0-60mph:</td><td>180 sec.</td></tr>
+	<tr><td>Engine:</td><td>Inline 4 2.0L</td></tr>
+	<tr><td>Colors:</td><td>Lime green, pale yellow</td></tr>
+</table>
+
+<h4>SG5 "Awesome"</h4>
+<table>
+	<tr><td>0-60mph:</td><td>1.8 sec.</td></tr>
+	<tr><td>Engine:</td><td>V12 8.3L</td></tr>
+	<tr><td>Colors:</td><td>Fire Engine Red, Gloss Black</td></tr>
+</table>
+
 @endsection
